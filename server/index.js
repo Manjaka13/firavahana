205a1fe5405348ab1a68ef58f8236a5662e675a5
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-03-01 17:00:28 +0100

    Server now serves the client static

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const Express = require("express");
 const cors = require("cors");
+const path = require("path");
 //const v1 = require("./api-v1");
 
 const app = Express();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3300;
 app.use(cors());
 app.use(Express.urlencoded({extended: true}));
 app.use(Express.json());
+app.use("../client/build", Express.static(path.join(__dirname, html_dir)));
 //app.use(v1.path, v1.route);
 app.get("/", (req, res) => {
 	res.json("Firavahana server side.");
